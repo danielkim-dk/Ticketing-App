cd0bbc1c8d2f6f17fcba9b564eba0b51d5aa86ac
danielkim-dk <danielkim.sung@gmail.com>
2024-02-26 00:33:58 -0800

    changed resolved page to client as well

--- app/admin/resolved/page.js
+++ app/admin/resolved/page.js
@@ -1,14 +1,26 @@
-import React from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
 import TicketTable from '@/app/components/TicketTable';
 import { getResolvedTickets } from '@/app/actions';
 
-const CurrentPage = async () => {
-  const tickets = await getResolvedTickets();
+const CurrentPage = () => {
+  const [tickets, setTickets] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      const fetchedTickets = await getResolvedTickets();
+      setTickets(fetchedTickets);
+      setIsLoading(false);
+    };
+    fetchTickets();
+  }, []);
+
   return (
     <div className="flex flex-col items-center h-full">
       <div className="p-12 font-bold">Resolved Page</div>
       <div className="flex items-center w-full">
-        {!tickets ? <div>Loading...</div> : <TicketTable tickets={tickets} />}
+        {!isLoading ? <div>Loading...</div> : <TicketTable tickets={tickets} />}
       </div>
     </div>
   );
