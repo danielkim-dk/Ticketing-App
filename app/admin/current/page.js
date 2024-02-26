9fad7e7fe1ab17ddd789d80ea51e698420d52959
danielkim-dk <danielkim.sung@gmail.com>
2024-02-26 00:23:24 -0800

    changed admin/current page to client rendered

--- app/admin/current/page.js
+++ app/admin/current/page.js
@@ -1,18 +1,26 @@
-import React from 'react';
+'use client';
+import React, { useState, useEffect } from 'react';
 import TicketTable from '@/app/components/TicketTable';
 import { getTickets } from '@/app/actions';
 
-const CurrentPage = async () => {
-  const tickets = await getTickets();
+const CurrentPage = () => {
+  const [tickets, setTickets] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      const fetchedTickets = await getTickets();
+      setTickets(fetchedTickets);
+      setIsLoading(false);
+    };
+    fetchTickets();
+  }, []);
+
   return (
     <div className="flex flex-col items-center h-full">
       <div className="p-12 font-bold">Current Page</div>
       <div className="flex items-center w-full">
-        {tickets === null ? (
-          <div>Loading...</div>
-        ) : (
-          <TicketTable tickets={tickets} />
-        )}
+        {isLoading ? <div>Loading...</div> : <TicketTable tickets={tickets} />}
       </div>
     </div>
   );
